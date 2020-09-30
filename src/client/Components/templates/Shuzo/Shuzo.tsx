5812e58407806c378ae439df0d8bdd8a82bf69e6
Kanae.Yamazaki <kanae.jp81@gmail.com>
2020-09-30 22:38:06 +0900

    修造ページの修正

--- src/client/Components/templates/Shuzo/Shuzo.tsx
+++ src/client/Components/templates/Shuzo/Shuzo.tsx
@@ -1,64 +1,10 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { getUsername } from '../../../reducks/users/selectors';
-
 import Header from '../../organisms/Header/Header';
 import shuzoList from '../../../Data/shuzo.json';
-import styled from 'styled-components'
-
 import shuzoImg from '../../../Images/shuzo.jpg'
-
-const Title = styled.h2`
-	font-size: 1.8rem;
-	color: #fff;
-`
-const Button = styled.p`
-	display: inline-block;
-	font-size: 1.6rem;
-	background: palevioletred;
-  border-radius: 30px;
-	border: 2px solid palevioletred;
-  color: #fff;
-	padding: 10px 20px;
-	margin: 0;
-	cursor: pointer;
-	transition: all  0.5s ease;
-	&:hover {
-		opacity: 0.8;
-		background: #fff;
-		color: palevioletred;
-	}
-`
-const Main = styled.div`
-	width: 90%;
-	margin: 0 auto;
-	margin-top: 20px;
-`
-const MainText = styled.p`
-	font-size: 2.4rem;
-	font-weight: bold;
-	font-family: 'Sawarabi Mincho', sans-serif;
-	padding 20px;
-	background: #fff;
-	opacity:0.8;
-	border-radius: 10px;
-	margin: 0 auto;
-`
-const Img = styled.img`
-	position: absolute;
-	top: 0;
-	left:0;
-	width: 100%;
-	height: 100%;
-	z-index: -1;
-`
-
-const Container = styled.div`
-	position: fixed;
-	bottom: 10px;
-	left 50%;
-	transform: translateX(-50%);
-`
+import styles from './shuzo.module.scss';
 
 const Shuzo = () => {
 	const selector = useSelector((state: any) => state);
@@ -72,20 +18,24 @@ const Shuzo = () => {
 	}
 
 	return (
-		<React.Fragment>
+		<>
+			<img src={shuzoImg} className={styles.bgImg}/>
 			<Header />
-			<Img src={shuzoImg} />
-			<Title>修造はいつでも{username}さんの味方！</Title>
-			<Main>
-				<MainText>{shuzo}</MainText>
-			</Main>
-			<Container>
-				<Button
-					onClick={() => shuzoSlot()}>
-					おかわり!
-				</Button>
-			</Container>
-		</React.Fragment>
+			<div className={styles.shuzo}>
+				<div className={styles.shuzo__inner}>
+					<h2 className={styles.shuzo__title}>修造はいつでも{username}さんの味方！</h2>
+					<main className={styles.shuzo__main}>
+						<p className={styles.shuzo__mainText}>{shuzo}</p>
+					</main>
+					<div className={styles.shuzo__container}>
+						<button className={styles.shuzo__button}
+							onClick={() => shuzoSlot()}>
+							おかわり!
+						</button>
+					</div>
+				</div>
+			</div>
+		</>
 	);
 };
 
