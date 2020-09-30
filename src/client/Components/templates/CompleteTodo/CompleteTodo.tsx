78d2f9db67ca4192721947da9d405e45ea1f0cec
Kanae.Yamazaki <kanae.jp81@gmail.com>
2020-09-30 23:13:02 +0900

    完了ページの修正

--- src/client/Components/templates/CompleteTodo/CompleteTodo.tsx
+++ src/client/Components/templates/CompleteTodo/CompleteTodo.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getUsername, getUserId } from '../../../reducks/users/selectors';
-import { push } from 'connected-react-router';
+import { getUserId } from '../../../reducks/users/selectors';
 import { listenTodos, deletetodo, returntodo } from '../../../reducks/todo/operations';
 import {
 	Table,
@@ -16,12 +15,12 @@ import { Delete, Replay } from '@material-ui/icons';
 import { getTodos } from '../../../reducks/todo/selectors';
 
 import Header from '../../organisms/Header/Header';
+import styles from './complete.module.scss';
 
 const CompleteTodo = () => {
 	const dispatch = useDispatch();
 	const selector = useSelector((state: any) => state);
 	const todos = getTodos(selector);
-	const username = getUsername(selector);
 	const uid = getUserId(selector);
 	useEffect(() => {
 		dispatch(listenTodos(uid))
@@ -29,72 +28,66 @@ const CompleteTodo = () => {
 	return (
 		<React.Fragment>
 			<Header />
-			<h2>Todo</h2>
-			<p>
-				{username}さんの完了リスト
-				<Box component="span" m={2}>
-					<Button
-						size="large"
-						variant="contained"
-						color="primary"
-						onClick={() => dispatch(push('/todo'))}>
-						Todo
-					</Button>
-				</Box>
-			</p>
-			<Table>
-				<TableHead>
-					<TableRow>
-						<TableCell>完了日</TableCell>
-						<TableCell>タイトル</TableCell>
-						<TableCell>内容</TableCell>
-						<TableCell></TableCell>
-					</TableRow>
-				</TableHead>
-				<TableBody>
-					{todos.todos.map((value: any) => {
-						const seconds = value.updated_at.seconds;
-						if (value.status === 'completed') {
-							const ts = seconds;
-							const d = new Date(ts * 1000);
-							const year = d.getFullYear();
-							const month = d.getMonth() + 1;
-							const day = d.getDate();
-							const date = (year + '-' + month + '-' + day);
-							return (
-								<TableRow key={value.id}>
-									<TableCell>{date}</TableCell>
-									<TableCell>{value.title}</TableCell>
-									<TableCell>{value.text}</TableCell>
-									<TableCell>
-										<Box component="span" m={1}>
-											<Button
-												variant="outlined"
-												color="primary"
-												startIcon={<Replay />}
-												onClick={() => dispatch(returntodo(uid, value.id))}>
-												戻す
-											</Button>
-										</Box>
-										<Box component="span" m={1}>
-											<Button
-												variant="outlined"
-												color="secondary"
-												startIcon={<Delete />}
-												onClick={() =>
-													dispatch(deletetodo(uid, value.id))
-												}>
-												削除
-											</Button>
-										</Box>
-									</TableCell>
+			<div className={styles.complete}>
+				<div className={styles.complete__inner}>
+					<h2 className={styles.complete__title}>TODO完了リスト</h2>
+					<div className={styles.complete__content}>
+						<Table>
+							<TableHead>
+								<TableRow>
+									<TableCell>完了日</TableCell>
+									<TableCell>タイトル</TableCell>
+									<TableCell>内容</TableCell>
+									<TableCell></TableCell>
 								</TableRow>
-							);
-						}
-						return;
-					})}
-				</TableBody>
-			</Table>
+							</TableHead>
+							<TableBody>
+								{todos.todos.map((value: any) => {
+									const seconds = value.updated_at.seconds;
+									if (value.status === 'completed') {
+										const ts = seconds;
+										const d = new Date(ts * 1000);
+										const year = d.getFullYear();
+										const month = d.getMonth() + 1;
+										const day = d.getDate();
+										const date = (year + '-' + month + '-' + day);
+										return (
+											<TableRow key={value.id}>
+												<TableCell>{date}</TableCell>
+												<TableCell>{value.title}</TableCell>
+												<TableCell>{value.text}</TableCell>
+												<TableCell>
+													<Box component="span" m={1}>
+														<Button
+															variant="outlined"
+															color="primary"
+															startIcon={<Replay />}
+															onClick={() => dispatch(returntodo(uid, value.id))}>
+															戻す
+														</Button>
+													</Box>
+													<Box component="span" m={1}>
+														<Button
+															variant="outlined"
+															color="secondary"
+															startIcon={<Delete />}
+															onClick={() =>
+																dispatch(deletetodo(uid, value.id))
+															}>
+															削除
+														</Button>
+													</Box>
+												</TableCell>
+											</TableRow>
+										);
+									}
+									return;
+								})}
+							</TableBody>
+						</Table>
+					</div>
+				</div>
+			</div>
 		</React.Fragment>
 	);
 };
